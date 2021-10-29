9b3dd213ff06f101c8eb1c71a20375032a72b4ea
Arthur Yan <arthuryan08@gmail.com>
2021-10-29 07:23:53 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet } from 'react-native'
+import {SafeAreaView, View, Text, StyleSheet} from 'react-native';
 
 export default () => {
   return (
     <SafeAreaView style={styles.page}>
       <Text style={styles.texto}>Salve família!!!</Text>
+      <Text style={styles.texto}>Segundo texto</Text>
+      <Text style={[styles.texto, {fontSize: 35}]}>Terceiro texto</Text>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  page:{
-    height: 200,
-    width: 200,
+  page: {
+    height: 250,
+    width: 250,
     backgroundColor: '#FF0000',
   },
-  texto:{
+  texto: {
     color: '#FFF',
-    fontSize:25,
-  }
-})
+    fontSize: 25,
+  },
+});
